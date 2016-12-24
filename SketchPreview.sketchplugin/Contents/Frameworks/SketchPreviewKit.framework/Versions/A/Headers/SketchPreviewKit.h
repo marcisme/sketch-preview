ed3830084695cae7a64d34e4753e7f18112f1c34
Marc Schwieterman <marc@marcisme.com>
2016-12-24 10:40:51 -0500

    Copy framework

---
+++ SketchPreview.sketchplugin/Contents/Frameworks/SketchPreviewKit.framework/Versions/A/Headers/SketchPreviewKit.h
@@ -0,0 +1,19 @@
+//
+//  SketchPreviewKit.h
+//  SketchPreviewKit
+//
+//  Created by Marc Schwieterman on 11/26/16.
+//  Copyright © 2016 Marc Schwieterman Software, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SketchPreviewKit.
+FOUNDATION_EXPORT double SketchPreviewKitVersionNumber;
+
+//! Project version string for SketchPreviewKit.
+FOUNDATION_EXPORT const unsigned char SketchPreviewKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SketchPreviewKit/PublicHeader.h>
+
+
